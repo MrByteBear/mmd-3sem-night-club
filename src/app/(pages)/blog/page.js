da3a5d8111c4a23a735991840d8ca383eb2aa8d8
pekr0002 <pekr0002@stud.kea.dk>
2025-12-02 12:31:05 +0100

    add base PageNav

--- src/app/(pages)/blog/page.js
+++ src/app/(pages)/blog/page.js
@@ -11,15 +11,15 @@ export default function Home() {
         style={{ backgroundImage: `url(${patternBg.src})` }}
       >
         <ul className="">
-          <li className="">
+          <li className="bg-background mb-8">
             <img src="https://cataas.com/cat" alt="" />
-            <h2 className="text-2xl uppercase">
+            <h2 className="text-2xl font-medium tracking-[0.48px] uppercase">
               More than 20 years of night club
             </h2>
-            <p className="text-accent uppercase">
+            <p className="text-accent font-medium tracking-[0.36px] uppercase">
               BY: Admin / 3 Comments / 16 Nov 2018
             </p>
-            <p>
+            <p className="text-[16px] leading-6 font-medium tracking-[0.32px]">
               Contrary to popular belief, Lorem Ipsum is not simply random text.
               It has roots in a piece of classical Latin literature from 45 BC,
               making it over 2000 years old. Richard McClintock, a Latin
@@ -37,60 +37,9 @@ export default function Home() {
               </div>
             </div>
           </li>
-          <li className="">
-            <img src="https://cataas.com/cat" alt="" />
-            <h2 className="text-2xl uppercase">
-              More than 20 years of night club
-            </h2>
-            <p className="text-accent uppercase">
-              BY: Admin / 3 Comments / 16 Nov 2018
-            </p>
-            <p>
-              Contrary to popular belief, Lorem Ipsum is not simply random text.
-              It has roots in a piece of classical Latin literature from 45 BC,
-              making it over 2000 years old. Richard McClintock, a Latin
-              professor at Hampden-Sydney College in Virginia, looked up one of
-              the more obscure Latin words, consectetur, from a Lorem Ipsum
-              passage, and going through the cites of the word in classical
-              literature, discovered the undoubtable source.
-            </p>
-            {/* btn */}
-            <div className="flex items-center justify-center pt-6">
-              <div className="inline-flex w-44 items-center justify-center gap-2.5 border-t-2 border-b-2 border-white px-4 py-5">
-                <div className="justify-start font-['Ubuntu'] text-lg font-medium tracking-tight text-white uppercase">
-                  Read more
-                </div>
-              </div>
-            </div>
-          </li>
-          <li className="">
-            <img src="https://cataas.com/cat" alt="" />
-            <h2 className="text-2xl uppercase">
-              More than 20 years of night club
-            </h2>
-            <p className="text-accent uppercase">
-              BY: Admin / 3 Comments / 16 Nov 2018
-            </p>
-            <p>
-              Contrary to popular belief, Lorem Ipsum is not simply random text.
-              It has roots in a piece of classical Latin literature from 45 BC,
-              making it over 2000 years old. Richard McClintock, a Latin
-              professor at Hampden-Sydney College in Virginia, looked up one of
-              the more obscure Latin words, consectetur, from a Lorem Ipsum
-              passage, and going through the cites of the word in classical
-              literature, discovered the undoubtable source.
-            </p>
-            {/* btn */}
-            <div className="flex items-center justify-center pt-6">
-              <div className="inline-flex w-44 items-center justify-center gap-2.5 border-t-2 border-b-2 border-white px-4 py-5">
-                <div className="justify-start font-['Ubuntu'] text-lg font-medium tracking-tight text-white uppercase">
-                  Read more
-                </div>
-              </div>
-            </div>
-          </li>
+
+          <PageNav />
         </ul>
-        <PageNav />
       </main>
     </div>
   );
--- src/app/components/(penny)/PageNav.jsx
+++ src/app/components/(penny)/PageNav.jsx
@@ -1,5 +1,14 @@
 const PageNav = () => {
-  return <> 1 2 3 næste side</>;
+  return (
+    <div className="flex w-full justify-center py-8">
+      <div className="flex w-[120px] justify-around">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>next {">"}</button>
+      </div>
+    </div>
+  );
 };
 
 export default PageNav;
