60a55a72247fe114cc58517e617ed38f219deb8f
Meleese Kærhavn <meleesekaerhavn@gmail.com>
2025-12-02 16:54:28 +0100

    contact form works and validates. I cant get the costum mrgins grid etc to work though

---
+++ src/app/(pages)/contact/page.js
@@ -0,0 +1,12 @@
+import ContactForm from "@/app/components/(Meleese)/ContactForm";
+
+export default function Contact() {
+
+  return (
+    <main>
+      <div className="col-start-2">
+        <ContactForm />
+      </div>
+    </main>
+  );
+}
