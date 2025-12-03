e4fcd87db25b08c8b212437b6f2794c756136305
Meleese Kærhavn <meleesekaerhavn@gmail.com>
2025-12-03 15:43:38 +0100

    Subheader components looks good and works fine. implemented on contact and table

--- src/app/(pages)/contact/page.js
+++ src/app/(pages)/contact/page.js
@@ -1,9 +1,11 @@
 import ContactForm from "@/app/components/(Meleese)/ContactForm";
+import SubHeader from "@/app/components/(Meleese)/SubHeader";
 
 export default function Contact() {
 
   return (
     <main>
+        <SubHeader title="Contact Us" />
       <div className="col-start-2">
         <ContactForm />
       </div>
