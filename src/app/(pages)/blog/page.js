66f12f58438bbfb178350224539c1e522306555c
pekr0002 <pekr0002@stud.kea.dk>
2025-12-04 11:16:03 +0100

    add header components

--- src/app/(pages)/blog/page.js
+++ src/app/(pages)/blog/page.js
@@ -1,16 +1,20 @@
 import BlogList from "@/app/components/(penny)/BlogList";
 import patternBg from "@/app/assets/bg/pattern_bg.jpg";
 import PageNav from "@/app/components/(penny)/PageNav";
-
+import HeaderNav from "@/app/components/(bjorn)/header-elem/HeaderNav";
+import SubHeader from "@/app/components/(Meleese)/SubHeader";
 export default function Home() {
   return (
     <div>
       {/* subpage hero */}
+      <HeaderNav />
+      <SubHeader title="Blog" />
+
       <main
         className="bg-cover bg-center"
         style={{ backgroundImage: `url(${patternBg.src})` }}
       >
-        <BlogList  />
+        <BlogList />
 
         <PageNav />
       </main>
