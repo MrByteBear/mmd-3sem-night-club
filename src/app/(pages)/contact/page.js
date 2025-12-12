bff035681dbf5d3ad4dce0f13792c48744c8b080
pekr0002 <pekr0002@stud.kea.dk>
2025-12-12 16:04:28 +0100

    add new component

--- src/app/(pages)/contact/page.js
+++ src/app/(pages)/contact/page.js
@@ -1,21 +1,17 @@
-import ContactForm from "@/app/components/(Meleese)/ContactForm";
 import SubHeader from "@/app/components/(Meleese)/SubHeader";
 import HeaderNav from "@/app/components/(Bjorn)/header-elem/HeaderNav";
-import FooterBox from "@/app/components/(Bjorn)/footer-elem/FooterBox";
-
+import ContactReactForm from "@/app/components/(penny)/ContactReactForm";
 
 export default function Contact() {
-
   return (
-        <div>
-        <HeaderNav/>
-    <main>
+    <div>
+      <HeaderNav />
+      <main>
         <SubHeader title="Contact Us" />
-      <div className="col-start-2">
-        <ContactForm />
-      </div>
-    </main>
-    {/* <FooterBox /> */}
+        <div className="col-start-2">
+          <ContactReactForm />
+        </div>
+      </main>
     </div>
   );
 }
