0cf71598506f5a81a694fa3f0cc594b287f279f9
pekr0002 <pekr0002@stud.kea.dk>
2025-12-15 08:57:14 +0100

    comments blogpostcomments

--- src/app/components/(penny)/BlogPostComments.jsx
+++ src/app/components/(penny)/BlogPostComments.jsx
@@ -1,14 +1,16 @@
 import { Suspense } from "react";
 
 const BlogPostComments = ({ id, commentCount }) => {
-  console.log("BlogPostComments received id:", id);
   return (
     <>
       <h2 className="mt-[89px] mb-11 pl-3 text-[32px] font-bold uppercase">
+        {/* change comment header based on how many comments there is */}
         {commentCount} comment{commentCount !== 1 ? "s" : ""}
       </h2>
+      {/* suspense for loading state */}
       <Suspense fallback={<div>Loading comments...</div>}>
         <ul>
+          {/* makes sure its only the comments from the right id that gets fetched */}
           <FetchComments id={id} />
         </ul>
       </Suspense>
@@ -17,15 +19,17 @@ const BlogPostComments = ({ id, commentCount }) => {
 };
 
 const FetchComments = async ({ id }) => {
-  console.log("Fetching comments for blog post ID:", id);
   const url = `http://localhost:4000/comments?blogpostId=${id}`;
+  // disable caching to always get latest comments
   const response = await fetch(url, { cache: "no-store" });
   const comments = await response.json();
 
+  // if no comments, show message
   if (!Array.isArray(comments) || comments.length === 0) {
     return <div>No comments yet</div>;
   }
 
+  // format date to dd mm yyyy
   return comments.map((post) => {
     const formattedDate = new Date(post.date).toLocaleDateString("en-GB", {
       day: "2-digit",
@@ -34,6 +38,7 @@ const FetchComments = async ({ id }) => {
     });
 
     return (
+      // each comment
       <li key={post.id} className="mb-[54px] pb-8">
         <h3 className="mb-6 text-[18px] font-medium tracking-[0.36px]">
           {post.name} -
