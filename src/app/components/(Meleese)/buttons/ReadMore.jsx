43042b36cd1ba2eb47bd2ae56e5610a8f2047a12
pekr0002 <pekr0002@stud.kea.dk>
2025-12-15 15:22:34 +0100

    btn styling

--- src/app/components/(Meleese)/buttons/ReadMore.jsx
+++ src/app/components/(Meleese)/buttons/ReadMore.jsx
@@ -1,13 +1,14 @@
 const ReadMore = () => {
-    return ( <div className="md:col-span-2 flex justify-end">
-          <button
-            type="submit"
-            className="px-10 py-3 mx-2 text-sm font-semibold tracking-wide uppercase border-t-4 border-b-4 hover:bg-white hover:text-black transition"
-            >
-            Read More
-          </button>
-        </div> 
-        ); 
-}
- 
+  return (
+    <div className="flex justify-end md:col-span-2">
+      <button
+        type="submit"
+        className="mx-2 border-t-2 border-b-2 px-10 py-3 text-sm font-semibold tracking-wide uppercase transition hover:bg-white hover:text-black"
+      >
+        Read More
+      </button>
+    </div>
+  );
+};
+
 export default ReadMore;
