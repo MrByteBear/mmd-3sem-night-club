8ffe2e319ae03255520d28641e99b28d856d1066
pekr0002 <pekr0002@stud.kea.dk>
2025-12-15 09:04:18 +0100

    comments paginator

--- src/app/components/(penny)/Paginator.jsx
+++ src/app/components/(penny)/Paginator.jsx
@@ -1,8 +1,9 @@
 "use client";
-
+// Next imports
 import { useRouter, useSearchParams } from "next/navigation";
 
 export default function Paginator({ totalPages }) {
+  // get current page from search params
   const router = useRouter();
   const searchParams = useSearchParams();
   const currentPage = Number(searchParams.get("page")) || 1;
@@ -11,17 +12,18 @@ export default function Paginator({ totalPages }) {
   if (totalPages <= 1) {
     return null;
   }
-
-  
+  // navigate to page on click
   function handleClick(pageNum) {
     router.push(`/blog?page=${pageNum}`);
   }
 
   return (
     <>
+      {/* create an a array based on total pages */}
       {Array.from({ length: totalPages }, (_, i) => i + 1).map((pageNum) => (
         <li key={pageNum}>
           <button
+            // navigate to page on click
             onClick={() => handleClick(pageNum)}
             className={`cursor-pointer ${currentPage === pageNum ? "text-accent font-bold" : ""}`}
           >
