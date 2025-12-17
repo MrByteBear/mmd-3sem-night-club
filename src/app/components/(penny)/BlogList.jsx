a2532cd907fd6a8f254e450484fa1d330da08a06
pekr0002 <pekr0002@stud.kea.dk>
2025-12-17 13:03:03 +0100

    blog list comments

--- src/app/components/(penny)/BlogList.jsx
+++ src/app/components/(penny)/BlogList.jsx
@@ -1,6 +1,7 @@
-// import components
+// import next components
 import Link from "next/link";
 import Image from "next/image";
+// component imports
 import Paginator from "./Paginator";
 import ReadMore from "@/app/components/(meleese)/buttons/ReadMore";
 
